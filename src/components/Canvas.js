cbaf9493b9c41cd2156c6ca0d089e60814576a8d
Joonas Pelttari <joonas.pelttari98@gmail.com>
2023-09-02 23:12:34 +0300

    pixel color editing

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -9,10 +9,30 @@ const URL = "https://l0c06d1a03.execute-api.eu-north-1.amazonaws.com"+
 const Canvas = () => {
   const [pixels, setPixels] = useState([]);
   const [highlightedPixelIdx, setHighlightedPixelIdx] = useState(-1);
+  const [text, setText] = useState("");
 
   const getData = async () => {
     const res = await axios.get(URL, {params: {"canvas": 1}});
     setPixels(res?.data);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axios.get(URL, {
+        params: {
+          "index": highlightedPixelIdx,
+          "hex": text,
+        }
+      });
+      await getData();
+    } catch (error) {
+      console.warn("color change FAILED");
+    }
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
   }
 
   const selectPixel = (index) => {
@@ -29,7 +49,15 @@ const Canvas = () => {
       {pixels.map((pixel, index) => (
       <Pixel key={index} index={index} color={pixel}
         highlightIdx={highlightedPixelIdx} selectPixel={selectPixel}/>
-      ))}  
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          HexColor:
+          <input type="text" value={text} onChange={handleChange}/>
+        </label>
+        <input type="submit" value="Change Color" />
+      </form>
     </div>
   );
 };
